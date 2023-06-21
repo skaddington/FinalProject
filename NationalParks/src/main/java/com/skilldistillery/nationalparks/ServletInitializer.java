e1238707d37e47455c7ac4f5e61d593edd0cf9ec
skaddington <skaddington92@gmail.com>
2023-06-21 10:50:30 -0600

    Boot project stubbed out

---
+++ NationalParks/src/main/java/com/skilldistillery/nationalparks/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nationalparks;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NationalParksApplication.class);
+	}
+
+}
---
+++ NationalParks/src/test/java/com/skilldistillery/nationalparks/NationalParksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nationalparks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalParksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
