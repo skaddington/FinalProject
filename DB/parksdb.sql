e7bde1f577b53ec51b05a9cfbd3558240637aea1
patrickhansen <hansen.patrick.d@gmail.com>
2023-06-21 18:08:50 -0600

    database schema approved

--- DB/parksdb.sql
+++ DB/parksdb.sql
@@ -26,8 +26,317 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(250) NOT NULL,
   `enabled` TINYINT NOT NULL,
   `role` VARCHAR(45) NULL,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `description` TEXT NULL,
+  `created_at` DATETIME NULL,
+  `updated_at` DATETIME NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park` ;
+
+CREATE TABLE IF NOT EXISTS `park` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(150) NOT NULL,
+  `date_established` DATE NOT NULL,
+  `description` TEXT NOT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `website_url` VARCHAR(2000) NULL,
+  `street` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `zip` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `activity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `activity` ;
+
+CREATE TABLE IF NOT EXISTS `activity` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `enabled` TINYINT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park_comment` ;
+
+CREATE TABLE IF NOT EXISTS `park_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `park_id` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  `reply_to_id` INT NULL,
+  `created_at` DATETIME NULL,
+  `enabled` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user-comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_user-comment_park1_idx` (`park_id` ASC),
+  INDEX `fk_user_comment_user_comment1_idx` (`reply_to_id` ASC),
+  CONSTRAINT `fk_user-comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user-comment_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_comment_user_comment1`
+    FOREIGN KEY (`reply_to_id`)
+    REFERENCES `park_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `attraction`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `attraction` ;
+
+CREATE TABLE IF NOT EXISTS `attraction` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NOT NULL,
+  `created_at` DATETIME NULL,
+  `updated_at` DATETIME NULL,
+  `park_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `website_url` VARCHAR(2000) NULL,
+  `enabled` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_local_recommendation_park1_idx` (`park_id` ASC),
+  INDEX `fk_local_recommendation_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_local_recommendation_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_local_recommendation_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park_has_activity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park_has_activity` ;
+
+CREATE TABLE IF NOT EXISTS `park_has_activity` (
+  `park_id` INT NOT NULL,
+  `activity_id` INT NOT NULL,
+  PRIMARY KEY (`park_id`, `activity_id`),
+  INDEX `fk_park_has_activity_activity1_idx` (`activity_id` ASC),
+  INDEX `fk_park_has_activity_park1_idx` (`park_id` ASC),
+  CONSTRAINT `fk_park_has_activity_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_park_has_activity_activity1`
+    FOREIGN KEY (`activity_id`)
+    REFERENCES `activity` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park_photo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park_photo` ;
+
+CREATE TABLE IF NOT EXISTS `park_photo` (
+  `id` INT NOT NULL,
+  `park_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `image_url` VARCHAR(2000) NOT NULL,
+  `image_date` DATE NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_photo_gallery_park1_idx` (`park_id` ASC),
+  INDEX `fk_photo_gallery_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_photo_gallery_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_photo_gallery_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `state`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `state` ;
+
+CREATE TABLE IF NOT EXISTS `state` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `abbreviation` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park_has_state`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park_has_state` ;
+
+CREATE TABLE IF NOT EXISTS `park_has_state` (
+  `park_id` INT NOT NULL,
+  `state_id` INT NOT NULL,
+  PRIMARY KEY (`park_id`, `state_id`),
+  INDEX `fk_park_has_state_state1_idx` (`state_id` ASC),
+  INDEX `fk_park_has_state_park1_idx` (`park_id` ASC),
+  CONSTRAINT `fk_park_has_state_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_park_has_state_state1`
+    FOREIGN KEY (`state_id`)
+    REFERENCES `state` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_favorites`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_favorites` ;
+
+CREATE TABLE IF NOT EXISTS `user_favorites` (
+  `user_id` INT NOT NULL,
+  `park_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `park_id`),
+  INDEX `fk_user_has_park_park1_idx` (`park_id` ASC),
+  INDEX `fk_user_has_park_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_park_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_park_park1`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `park_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `park_rating` ;
+
+CREATE TABLE IF NOT EXISTS `park_rating` (
+  `user_id` INT NOT NULL,
+  `park_id` INT NOT NULL,
+  `rating` INT NOT NULL,
+  `rating_comment` TEXT NULL,
+  `rating_date` DATETIME NULL,
+  PRIMARY KEY (`user_id`, `park_id`),
+  INDEX `fk_user_has_park_park2_idx` (`park_id` ASC),
+  INDEX `fk_user_has_park_user2_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_park_user2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_park_park2`
+    FOREIGN KEY (`park_id`)
+    REFERENCES `park` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `attraction_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `attraction_comment` ;
+
+CREATE TABLE IF NOT EXISTS `attraction_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  `reply_to_id` INT NULL,
+  `created_at` DATETIME NULL,
+  `attraction_id` INT NOT NULL,
+  `enabled` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user-comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_user_comment_user_comment1_idx` (`reply_to_id` ASC),
+  INDEX `fk_attraction_comment_attraction1_idx` (`attraction_id` ASC),
+  CONSTRAINT `fk_user-comment_user10`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_comment_user_comment10`
+    FOREIGN KEY (`reply_to_id`)
+    REFERENCES `attraction_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attraction_comment_attraction1`
+    FOREIGN KEY (`attraction_id`)
+    REFERENCES `attraction` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `attraction_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `attraction_rating` ;
+
+CREATE TABLE IF NOT EXISTS `attraction_rating` (
+  `user_id` INT NOT NULL,
+  `rating` INT NOT NULL,
+  `rating_comment` TEXT NULL,
+  `rating_date` DATETIME NULL,
+  `attraction_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `attraction_id`),
+  INDEX `fk_user_has_park_user2_idx` (`user_id` ASC),
+  INDEX `fk_attraction_rating_attraction1_idx` (`attraction_id` ASC),
+  CONSTRAINT `fk_user_has_park_user20`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attraction_rating_attraction1`
+    FOREIGN KEY (`attraction_id`)
+    REFERENCES `attraction` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -46,7 +355,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `parksdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `image_url`, `description`, `created_at`, `updated_at`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 COMMIT;
 
