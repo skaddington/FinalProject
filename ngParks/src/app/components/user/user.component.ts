ed64a471042baea547654f159358c1793accf7b4
skaddington <skaddington92@gmail.com>
2023-06-24 17:42:13 -0600

    User page with favorite list in progress

--- ngParks/src/app/components/user/user.component.ts
+++ ngParks/src/app/components/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Park } from 'src/app/models/park';
 import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 import { ParkService } from 'src/app/services/park.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -13,12 +14,13 @@ import { UserService } from 'src/app/services/user.service';
 export class UserComponent {
   users: User[] = [];
 
+  loggedInUser: User | null= null;
   editUser: User | null = null;
   selectedUser: User | null = null;
 
   constructor(
     private userService: UserService,
-    // private incompletePipe: IncompletePipe,
+    private authService:AuthService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
@@ -36,6 +38,26 @@ export class UserComponent {
       }
     }
     this.reload();
+  }
+
+  checkUser(): User | null {
+    this.authService.getLoggedInUser().subscribe({
+      next: (user) => {
+        return (this.loggedInUser = user);
+      },
+      error: (problem) => {
+        console.error('UserComponent.reload(): error loading Users');
+        console.error(problem);
+      },
+    });
+    if (this.loggedInUser) {
+      return this.loggedInUser;
+    }
+    return null;
+  }
+
+  logoutUser(){
+    this.loggedInUser = null;
   }
 
   reload(): void {
@@ -95,6 +117,8 @@ export class UserComponent {
       },
     });
   }
+
+  removeParkFromFavorites() {}
 
   // deletePark(parkId: number): void {
   //   this.parkService.destroy(parkId).subscribe({
