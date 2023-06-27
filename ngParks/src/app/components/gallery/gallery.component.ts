698de84884c306fbaecddf81735105364e800602
jennkal <jennkalkowski@gmail.com>
2023-06-27 14:31:53 -0500

    fixed gallery errors

--- ngParks/src/app/components/gallery/gallery.component.ts
+++ ngParks/src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,11 @@
+import { ParkService } from './../../services/park.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Park } from 'src/app/models/park';
+import { ParkPhoto } from 'src/app/models/park-photo';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-gallery',
@@ -6,5 +13,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent {
+
+  loggedInUser: User | null = null;
+  parks: Park[] = [];
+
+
+  constructor(
+    private parkService: ParkService,
+    private route: Router,
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.reload();
+  }
+
+  reload():void {
+    this.parkService.index().subscribe({
+      next: (parkList) => {
+        this.parks = parkList;
+      },
+      error: (boo) => {
+        console.error('ParkPhotoComponent.reload(): error loading Park Photos');
+        console.error(boo);
+      }
+    })
+  }
+
+  displayParkPhotos() {
+    return (this.parks)
+  }
 
 }
